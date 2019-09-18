7bc3594245a9fc1fbd2923926deccdffb73825a7
Jennifer Kirsch <j.kirsch@stic-online.de>
2019-09-18 11:51:56 +0200

    Added progress display.

--- hopping_frog.py
+++ hopping_frog.py
@@ -5,6 +5,7 @@ The frog always jumps a random number of lilly pads that is at least 1 and at mo
 last pad.
 How many jumps does it take on average for the frog to reach the end of the pond?
 """
+import math
 import random
 from builtins import input, int, ValueError, range, str
 
@@ -28,15 +29,17 @@ class Pond:
         """
         return self.frog_position == self.size
 
-    def do_hop(self):
+    def do_hop(self, do_print):
         """
         Gets a random number between the current position of our frog and the pond's size. The frog will then "jump"
         to that position.
 
+        :param bool do_print: Wheteher or not to print the current frog position.
         :return void:
         """
         self.frog_position = random.randint(self.frog_position + 1, self.size)
-        print("Frog is not at position " + str(self.frog_position) + " of " + str(self.size))
+        if do_print:
+            print("Frog is not at position " + str(self.frog_position) + " of " + str(self.size))
 
 
 def main():
@@ -55,18 +58,25 @@ def main():
     print("How often do you want to simulate the crossing?")
     sim_count = input()
     try:
-        sim_count = int(sim_count)
+        sim_count_int = int(sim_count)
     except ValueError:
-        sim_count = 100
+        sim_count_int = 100
+        sim_count = "100"
         print("Malformed input. Please only enter integers. Assuming the default of 100.")
+    print("Do you want to print the frog's positions? (y/N)")
+    do_print = input()
+    do_print = do_print == "y" or do_print == "Y"
     total_jumps = 0
-    for c in range(0, sim_count):
+    for c in range(0, sim_count_int):
         """Creates sim_count ponds and simulates the frog jumping for each one"""
+        print(
+            "Simulation " + str(c + 1) + " of " + sim_count + ". Progress: " + str(math.floor((c + 1) / sim_count_int *
+                                                                                              100)) + "%")
         pond = Pond(pond_size)
         while not pond.has_reached_end():
-            pond.do_hop()
+            pond.do_hop(do_print)
             total_jumps += 1
-    print("The average amount of jumps in " + str(sim_count) + " runs was: " + str(total_jumps / sim_count))
+    print("The average amount of jumps in " + sim_count + " runs was: " + str(total_jumps / sim_count_int))
 
 
 if __name__ == "__main__":
